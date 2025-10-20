097306e432e6e9a294ec20528aa4bbaae406b176
Jos de Jong <wjosdejong@gmail.com>
2025-10-20 15:20:16 +0200

    chore: fix missing import

--- src/lib/objects.ts
+++ src/lib/objects.ts
@@ -1,4 +1,4 @@
-import type { NestedObject, Path } from './types.d.ts'
+import type { GenericObject, NestedObject, Path } from './types.d.ts'
 
 // The utils are largely copied from:
 // - https://github.com/josdejong/csv42
