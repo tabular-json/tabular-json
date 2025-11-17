1f6aa25c9931ebb4bb3b5744bc737c15f02ffc1a
Jos de Jong <wjosdejong@gmail.com>
2025-11-17 21:36:43 +0100

    fix: broken exports in index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,12 +1,12 @@
 export { parse } from './parse.js'
 export { stringify, type StringifyOptions } from './stringify.js'
+export { isTabular } from './tabular.js'
 export {
   always,
-  isTabular,
   noNestedArrays,
   noNestedTables,
   isHomogeneous,
   noLongStrings
-} from './tabular.js'
+} from './tableProperties.js'
 
 export type { TabularData, OutputAsTable, NonEmptyArray } from './types.d.ts'
